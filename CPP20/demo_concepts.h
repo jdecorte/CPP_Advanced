6523773b4f0d62e6d384be3a2043a676d2b50296
Johan Decorte <johan.decorte@hogent.be>
2024-11-21 12:23:32 +0100

    concepts

--- CPP20/demo_concepts.h
+++ CPP20/demo_concepts.h
@@ -11,35 +11,28 @@ T multiply0(T first, T second) {return first * second;}
 // C++20 concept that constrains the template parameter to
 template<typename T>
    requires std::integral<T> || std::floating_point<T>
-T multiply(T first, T second) {return first * second;}
+T multiply1(T first, T second) {return first * second;}
+
+// C++20 custom concept that constrains the template parameter
+template<typename T>
+concept Numeric = std::integral<T> || std::floating_point<T>;
+
+auto multiply2(Numeric auto first, Numeric auto second) {return first * second;}
 
 void demo_concepts() {
 
-   std::cout << "C++17: Product of 5 and 3: " << multiply0(5, 3)
-   << "\nProduct of 7.25 and 2.0: " << multiply0(7.25, 2.0) << "\n";
+   std::cout << "C++17:\n Product of 5 and 3: " << multiply0(5, 3)
+   << "\n Product of 7.25 and 2.0: " << multiply0(7.25, 2.0) << "\n";
 
-   std::cout << "C++20: Product of 5 and 3: " << multiply(5, 3)
-      << "\nProduct of 7.25 and 2.0: " << multiply(7.25, 2.0) << "\n";
+   std::cout << "C++20:\n Product of 5 and 3: " << multiply1(5, 3)
+      << "\n Product of 7.25 and 2.0: " << multiply1(7.25, 2.0) << "\n";
+
+   std::cout << "C++20 abbreviated function template:\n Product of 5 and 3: " << multiply2(5, 3)
+      << "\n Product of 7.25 and 2.0: " << multiply2(7.25, 2.0) << "\n";
 
    std::string s1{"hi"};
    std::string s2{"bye"};
    // int s1{5};
    // int s2{3};
-   auto result{multiply0(s1, s2)};
+   // auto result{multiply0(s1, s2)};
 }
-
-
-/**************************************************************************
- * (C) Copyright 1992-2022 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- **************************************************************************/
